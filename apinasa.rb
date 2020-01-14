8f17d7c6ddc345b518ce40b0ae6ac8a229a18e9a
romell <osed.td@gmail.com>
2020-01-14 15:11:30 -0300

    prueba

---
+++ apinasa.rb
@@ -0,0 +1,45 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+require "json"
+require_relative 'cuerpohtml'
+
+def get_data(url)
+    url = URI(url)
+http = Net::HTTP.new(url.host, url.port)
+request = Net::HTTP::Get.new(url)
+http.use_ssl =  true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+response = http.request(request)
+JSON.parse(response.read_body)
+
+end
+
+clean_data = get_data ("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=jGjPvUgM7klVKhjGMYZNOyimurk8ZpYVtXkBcW2V")
+
+
+
+
+
+File.open('nasa.html','w') do |f|
+    
+    f.puts head
+    f.puts '<ul>'
+
+    clean_data['photos'][0..14].each do |hash_info|
+        f.puts "<li><img src='#{hash_info['img_src']}' alt='#{hash_info[' ']}'</li>"
+      end
+    f.puts '</ul>'
+    f.puts footer
+end
+`firefox nasa.html`
+   
+
+
+
+
+
+
+
+
+
---
+++ buid_web_page .rb
@@ -0,0 +1,24 @@
+def head
+    "<!DOCTYPE html>
+    <html lang='es'>
+     <head>
+     <meta charset='UTF-8'>
+     <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css' integrity='sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS' crossorigin='anonymous'>
+     <link rel='stylesheet' href='./style.css'>
+     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Fotos Nasa</title>\n
+    </head>\n
+  <body>\n
+  <header>
+  <h1> Trabajamos </h1>
+  <h2> Siempre para</h2>
+  <h2>    ud       </h2>
+  </header> "
+ end
+ 
+ def footer
+     "\t</body> \n
+        </html>"
+ 
+ end 
